da469a764cea47a9476f383b20a4bfe4623ba113
Your Name <you@example.com>
2022-04-20 10:18:17 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -186,7 +186,7 @@ Pair * nextTreeMap(TreeMap * tree) {
             } 
             tree->current = tree->current->parent;
         }
-        tree->current = tree->current->parent;
+        //tree->current = tree->current->parent;
         return NULL;
     }
 }
